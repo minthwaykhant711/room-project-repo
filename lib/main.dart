c245e44f04f4a00b60d94c7d317c20cb022fe12e
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-10-28 00:59:23 +0700

    room filter is not working well yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/staff_browsing.dart';
 import 'package:flutter_application_1/staff_dashboard.dart';
 
 void main() {
-  runApp(MaterialApp(home: StaffDashboard(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: StaffBrowsing(), debugShowCheckedModeBanner: false));
 }

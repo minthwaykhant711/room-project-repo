756a42ac93e50e08a4bf052c46772dcf7fba8f2f
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-10-28 22:35:01 +0700

    filter still doesn't work but let me commit it first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/lecturer_browsing.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(home: LecturerBrowsing()));
 }

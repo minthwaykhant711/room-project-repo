49a76545ed19b237271b048b9c7bbb2c713f51f9
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-10-29 00:38:38 +0700

    student multimedia room design finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/std_req_popup.dart';
 
 import 'package:flutter_application_1/student_browsing_me.dart';
+import 'package:flutter_application_1/student_browsing_multi.dart';
 
 void main() {
-  runApp(const MaterialApp(home: StudentBrowsingMe()));
+  runApp(const MaterialApp(home: StudentBrowsingMulti()));
 }

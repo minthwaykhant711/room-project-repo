4bcca2f97e7f436daf5a5382ff1f3f14d6277876
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-10-29 00:57:40 +0700

    finished study room browsing for student

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/student_browsing_me.dart';
 import 'package:flutter_application_1/student_browsing_multi.dart';
+import 'package:flutter_application_1/student_browsing_study.dart';
 
 void main() {
-  runApp(const MaterialApp(home: StudentBrowsingMulti()));
+  runApp(const MaterialApp(home: StudentBrowsingStudy()));
 }
